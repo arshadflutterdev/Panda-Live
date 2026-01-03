9fc4aa14dfc3a18efe0e7868db58b14146d642f0
arshadflutterdev <arshadcodess@gmail.com>
2026-01-03 13:03:38 +0500

    changes

--- lib/App/AppUi/splash_screen.dart
+++ lib/App/AppUi/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pandlive/Utils/Constant/app_heightwidth.dart';
+import 'package:pandlive/Utils/Constant/app_images.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +12,20 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double height = AppHeightwidth.screenHeight(context);
+    double width = AppHeightwidth.screenWidth(context);
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(AppImages.splashimage),
+          ),
+        ),
+      ),
+    );
   }
 }
