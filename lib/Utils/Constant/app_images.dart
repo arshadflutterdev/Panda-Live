fcc616b21178df641132bdb5ec153513d851eff2
arshadflutterdev <arshadcodess@gmail.com>
2026-01-04 21:25:37 +0500

    changes

--- lib/Utils/Constant/app_images.dart
+++ lib/Utils/Constant/app_images.dart
@@ -5,8 +5,8 @@ class AppImages {
   static String facebook = "assets/icons/facebook (1).png";
   static String google = "assets/icons/google (1).png";
   static String or = "assets/images/or.png";
-  static String phone = "assets/icons/phone.png";
-  static String email = "assets/icons/email (1).png";
+  static String phone = "assets/icons/smartphone.png";
+  static String email = "assets/icons/envelope.png";
   //below images for authOpt screen
   static String girl1 = "assets/images/girll1.jpg";
   static String girl2 = "assets/images/girll2.jpg";
