7187b9570e4cfd6afb5ddf0d3669232f4f5b1f75
arshadflutterdev <arshadcodess@gmail.com>
2026-01-07 23:54:49 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:get/get.dart';
 import 'package:pandlive/App/Routes/app_routes.dart';
+import 'package:pandlive/App/Services/Localization/app_localization.dart';
+import 'package:pandlive/Utils/Constant/app_style.dart';
+import 'package:pandlive/l10n/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,18 +24,46 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
+        AppLocalizations.delegate, // ✅ ADD THIS
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: [Locale("en"), Locale("ar")],
 
-      initialRoute: AppRoutes.splash,
-      getPages: AppRoutes.routes,
+      // initialRoute: AppRoutes.splash,
+      // getPages: AppRoutes.routes,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      home: ChangeLanguage(),
     );
   }
 }
+
+class ChangeLanguage extends StatefulWidget {
+  const ChangeLanguage({super.key});
+
+  @override
+  State<ChangeLanguage> createState() => _ChangeLanguageState();
+}
+
+class _ChangeLanguageState extends State<ChangeLanguage> {
+  @override
+  Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context)!;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(localizations.helloWorld)),
+      body: Center(
+        child: TextButton(
+          onPressed: () {},
+          child: Text("Langauge", style: AppStyle.logo),
+        ),
+      ),
+    );
+  }
+}
+
+void changeLocal(BuildContext context, Locale newLocale) {}
