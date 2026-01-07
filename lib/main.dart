2c349b5f5b457294f6cb9429f5eec9afca2b47da
arshadflutterdev <arshadcodess@gmail.com>
2026-01-07 14:25:39 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      initialRoute: AppRoutes.bottomnav,
+      initialRoute: AppRoutes.splash,
       getPages: AppRoutes.routes,
       title: 'Flutter Demo',
       theme: ThemeData(
