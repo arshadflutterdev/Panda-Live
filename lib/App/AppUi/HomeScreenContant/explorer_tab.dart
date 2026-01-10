08b01ff51bb28a749e622cee65fec776da4b3e4a
arshadflutterdev <arshadcodess@gmail.com>
2026-01-10 23:39:27 +0500

    changes

--- lib/App/AppUi/HomeScreenContant/explorer_tab.dart
+++ lib/App/AppUi/HomeScreenContant/explorer_tab.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
 import 'package:pandlive/Utils/Constant/app_colours.dart';
 import 'package:pandlive/Utils/Constant/app_images.dart';
+import 'package:pandlive/Utils/Constant/app_style.dart';
 
 class ExplorerScreen extends StatefulWidget {
   const ExplorerScreen({super.key});
@@ -18,19 +21,85 @@ class _ExplorerScreenState extends State<ExplorerScreen> {
     AppImages.egirl2,
     AppImages.egirl3,
     AppImages.eman1,
+    AppImages.egirl2,
     AppImages.egirl4,
     AppImages.egirl5,
     AppImages.egirl6,
     AppImages.eman0,
     AppImages.egirl2,
-    AppImages.egirl2,
     AppImages.egirl3,
     AppImages.eman1,
     AppImages.egirl4,
   ];
+  //live user list
+  List<String> usernames = [
+    "Ali",
+    "Emily",
+    "Usman",
+    "Sophia",
+    "Ayesha",
+    "Olivia",
+    "Hassan",
+    "Emma",
+    "Zara",
+    "Mia",
+    "Lily",
+    "Ahmed",
+    "Noor",
+    "Ava",
+    "Salman",
+    "Sofia",
+  ];
+  List<String> usernamesArabic = [
+    "علي", // Ali
+    "إميلي", // Emily
+    "عثمان", // Usman
+    "صوفيا", // Sophia
+    "عائشة", // Ayesha
+    "أوليفيا", // Olivia
+    "حسن", // Hassan
+    "إيما", // Emma
+    "زارا", // Zara
+    "ميا", // Mia
+    "ليلي", // Lily
+    "أحمد", // Ahmed
+    "نور", // Noor
+    "آفا", // Ava
+    "سلمان", // Salman
+    "صوفيا", // Sofia
+  ];
+
+  //list countries flag
+  List<String> countries = [
+    AppImages.pak,
+    AppImages.usa,
+    AppImages.saudi,
+    AppImages.uae,
+    AppImages.india,
+    AppImages.pak,
+    AppImages.usa,
+    AppImages.saudi,
+    AppImages.uae,
+    AppImages.india,
+    AppImages.pak,
+    AppImages.usa,
+    AppImages.saudi,
+    AppImages.uae,
+    AppImages.india,
+    AppImages.pak,
+  ];
+  //views
+  List<int> views = [10, 15, 20, 30, 5, 3, 25, 12, 18, 7, 22, 14, 9, 35, 8, 28];
+
   @override
   Widget build(BuildContext context) {
+    bool isArabic = Get.locale?.languageCode == "ar";
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.transparent,
+        onPressed: () {},
+        child: Image(image: AssetImage(AppImages.golive), color: Colors.white),
+      ),
       backgroundColor: Colors.white,
 
       body: GridView.builder(
@@ -64,13 +133,40 @@ class _ExplorerScreenState extends State<ExplorerScreen> {
                     ),
                     child: Padding(
                       padding: const EdgeInsets.all(2.0),
-                      child: Text("Live now"),
+                      child: Text(isArabic ? "عش الآن" : "Live now"),
                     ),
                   ),
                 ),
-                Row(children: [
-
-                  ],
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Row(
+                    children: [
+                      Text(
+                        isArabic ? usernamesArabic[index] : usernames[index],
+                        style: isArabic
+                            ? AppStyle.arabictext.copyWith(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              )
+                            : TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      Gap(5),
+                      Image(
+                        image: AssetImage(countries[index]),
+                        height: 20,
+                        width: 20,
+                      ),
+                      Spacer(),
+                      Icon(Icons.remove_red_eye, color: Colors.white),
+                      Gap(3),
+                      Text(
+                        views[index].toString(),
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
