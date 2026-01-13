7bc2342a481ef45fd9a04890ae7ce4eaa72a9a45
arshadflutterdev <arshadcodess@gmail.com>
2026-01-13 22:16:46 +0500

    changes

--- lib/Utils/Constant/app_images.dart
+++ lib/Utils/Constant/app_images.dart
@@ -63,4 +63,9 @@ class AppImages {
   static const note1 = "assets/images/note2 (1).png";
   static const note2 = "assets/images/note3 (1).png";
   static const note3 = "assets/images/note4 (1).png";
+  //info image ar
+  static const infoar = "assets/images/infroar.png";
+  static const infoar0 = "assets/images/infroar0.png";
+  static const infoar1 = "assets/images/infoar1.png";
+  static const infoar2 = "assets/images/infoar2.png";
 }
