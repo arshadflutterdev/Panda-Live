ff5c0f24162b3f93bf84c5b717e7a7bfdd2ab043
arshadflutterdev <arshadcodess@gmail.com>
2026-01-14 23:51:35 +0500

    changes

---
+++ lib/App/Authentication/EmailAuth/EmailAuthControllers/emailauth_controller.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class EmailauthController extends GetxController {
+  // Future<void> createAccountWithEmail() async {
+  //   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  //   firebaseAuth.createUserWithEmailAndPassword(email: email, password: password)
+  // }
+}
--- lib/App/Bindings/app_bindings.dart
+++ lib/App/Bindings/app_bindings.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
+import 'package:pandlive/App/Authentication/EmailAuth/EmailAuthControllers/emailauth_controller.dart';
 import 'package:pandlive/App/Authentication/GoogleAuth/google_auth_controller.dart';
 
 class AuthBindings extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<GoogleAuthController>(() => GoogleAuthController());
+    Get.lazyPut<EmailauthController>(() => EmailauthController());
   }
 }
