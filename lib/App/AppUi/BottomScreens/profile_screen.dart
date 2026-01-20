e61c2752bfd2152d0ca5422fbabac24b94b0b5a9
arshadflutterdev <arshadcodess@gmail.com>
2026-01-20 11:24:35 +0500

    changes

--- lib/App/AppUi/BottomScreens/profile_screen.dart
+++ lib/App/AppUi/BottomScreens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get_utils/get_utils.dart';
@@ -65,6 +66,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
   void dispose() {
     bgstream.cancel();
     super.dispose();
+  }
+
+  Future<void> getUserDetails() async {
+    final userdetails = FirebaseFirestore.instance
+        .collection("userProfile")
+        .doc()
+        .get();
   }
 
   @override
