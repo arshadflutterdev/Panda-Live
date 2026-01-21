caf01b1d3215f97e9bcee9c2730f306e18d1db27
arshadflutterdev <arshadcodess@gmail.com>
2026-01-21 13:08:33 +0500

    changes

--- lib/App/AppUi/LiveStreaming/GoLiveStream/golive_screen.dart
+++ lib/App/AppUi/LiveStreaming/GoLiveStream/golive_screen.dart
@@ -10,8 +10,19 @@ class GoliveScreen extends StatefulWidget {
 
 class _GoliveScreenState extends State<GoliveScreen> {
   late RtcEngine _engine;
+  final String appId = "5eda14d417924d9baf39e83613e8f8f5";
+  final String channelName = "testingChannel";
   Future<void> initAgoraEngine() async {
+    // create engine
     _engine = createAgoraRtcEngine();
+    //initlize engine with appId
+    _engine.initialize(
+      RtcEngineContext(
+        appId: appId,
+        channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+      ),
+    );
+    //Register the event handler to listen for callbacks
   }
 
   @override
