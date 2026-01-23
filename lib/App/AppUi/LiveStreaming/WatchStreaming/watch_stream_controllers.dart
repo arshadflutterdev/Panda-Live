35c290f1a934dada111ae295832c89afae6fa97f
arshadflutterdev <arshadcodess@gmail.com>
2026-01-23 15:52:50 +0500

    changes

--- lib/App/AppUi/LiveStreaming/WatchStreaming/watch_stream_controllers.dart
+++ lib/App/AppUi/LiveStreaming/WatchStreaming/watch_stream_controllers.dart
@@ -1,78 +1,135 @@
-import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class WatchStreamControllers extends GetxController {
-  //main code to join you as audiance
-  late RtcEngine engine;
-  final String appId = "5eda14d417924d9baf39e83613e8f8f5";
-  final String channelName = "testingChannel";
-  final String appToken =
-      "007eJxTYGiYa1Zzb+EvSU955zt1DCJf/iY+dlHPmzDDc4au6hT+KY4KDKapKYmGJikmhuaWRiYplkmJacaWqRbGZobGqRZpFmmmx/4VZjYEMjL8qb/BysgAgSA+H0NJanFJZl66c0ZiXl5qDgMDAH4iI9Q=";
+  RxBool isfollowing = false.obs;
+  final currenduser = FirebaseAuth.instance.currentUser!.uid;
+  TextEditingController commentController = TextEditingController();
 
-  var remoteviewController = Rxn<VideoViewController>();
-  var arg = Get.arguments;
-  Future<void> joinasaudi() async {
-    engine = createAgoraRtcEngine();
-    await engine.initialize(RtcEngineContext(appId: appId));
-    await engine.setClientRole(role: ClientRoleType.clientRoleAudience);
-    await engine.enableVideo();
-    engine.registerEventHandler(
-      RtcEngineEventHandler(
-        onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
-          if (remoteUid.toString() == arg["agoraUid"].toString()) {
-            remoteviewController.value = VideoViewController.remote(
-              rtcEngine: engine,
-              canvas: VideoCanvas(uid: remoteUid),
-              connection: connection,
-            );
-          }
-        },
+  final arg = Get.arguments;
+  Future<void> toggleFollow() async {
+    // 1. Instantly flip the UI button state
+    isfollowing.toggle();
 
-        onRemoteVideoStateChanged:
-            (connection, remoteUid, state, reason, elapsed) {
-              if (remoteUid.toString() == arg["agoraUid"].toString() &&
-                  state == RemoteVideoState.remoteVideoStateDecoding) {
-                if (remoteviewController.value == null) {
-                  remoteviewController.value = VideoViewController.remote(
-                    rtcEngine: engine,
-                    canvas: VideoCanvas(uid: remoteUid),
-                    connection: connection,
-                  );
-                }
-              }
-            },
-        onUserOffline:
-            (
-              RtcConnection connection,
-              int remoteUid,
-              UserOfflineReasonType reason,
-            ) {
-              if (remoteUid.toString() == arg["agoraUid"].toString()) {
-                remoteviewController.value = null;
+    // 2. Setup references for the Host (The person being watched)
+    var hostProfileRef = FirebaseFirestore.instance
+        .collection("userProfile")
+        .doc(arg["uid"]);
+    var hostFollowersSub = hostProfileRef
+        .collection("Followers")
+        .doc(currenduser);
 
-                Get.back();
-              }
-            },
-      ),
-    );
-    await engine.joinChannel(
-      token: appToken,
-      channelId: channelName,
-      uid: 0,
-      options: ChannelMediaOptions(
-        clientRoleType: ClientRoleType.clientRoleAudience,
-        publishCameraTrack: false,
-        publishMicrophoneTrack: false,
-        autoSubscribeAudio: true,
-        autoSubscribeVideo: true,
-      ),
-    );
+    // 3. Setup references for Me (The person watching)
+    var myProfileRef = FirebaseFirestore.instance
+        .collection("userProfile")
+        .doc(currenduser);
+    var myFollowingSub = myProfileRef.collection("Following").doc(arg["uid"]);
+
+    try {
+      if (isfollowing.value) {
+        // --- ACTION: FOLLOW ---
+
+        // Add me to the Host's "Followers" list
+        await hostFollowersSub.set({
+          "followerId": currenduser,
+          "followAt": FieldValue.serverTimestamp(),
+        });
+        // Add the Host to my "Following" list
+        await myFollowingSub.set({
+          "hostId": arg["uid"],
+          "followAt": FieldValue.serverTimestamp(),
+        });
+
+        // Update the totals on the main profile documents (Safe create if missing)
+        await hostProfileRef.set({
+          "totalFollowers": FieldValue.increment(1),
+        }, SetOptions(merge: true));
+        await myProfileRef.set({
+          "totalFollowing": FieldValue.increment(1),
+        }, SetOptions(merge: true));
+      } else {
+        // --- ACTION: UNFOLLOW ---
+        await hostFollowersSub.delete();
+        await myFollowingSub.delete();
+
+        // Get the current count first to make sure we don't go below 0
+        var hostDoc = await hostProfileRef.get();
+        int currentHostFollowers = hostDoc.data()?['totalFollowers'] ?? 0;
+
+        // Only decrement if the count is greater than 0
+        if (currentHostFollowers > 0) {
+          await hostProfileRef.set({
+            "totalFollowers": FieldValue.increment(-1),
+          }, SetOptions(merge: true));
+        } else {
+          // If it's already 0 or negative, force it to stay at 0
+          await hostProfileRef.set({
+            "totalFollowers": 0,
+          }, SetOptions(merge: true));
+        }
+
+        // Do the same for your "following" count
+        var myDoc = await myProfileRef.get();
+        int currentMyFollowing = myDoc.data()?['totalFollowing'] ?? 0;
+
+        if (currentMyFollowing > 0) {
+          await myProfileRef.set({
+            "totalFollowing": FieldValue.increment(-1),
+          }, SetOptions(merge: true));
+        } else {
+          await myProfileRef.set({
+            "totalFollowing": 0,
+          }, SetOptions(merge: true));
+        }
+      }
+    } catch (e) {
+      // If something goes wrong (no internet, etc), flip the button back
+      isfollowing.toggle();
+      debugPrint("Follow Error: $e");
+    }
   }
 
-  @override
-  void onClose() {
-    engine.leaveChannel();
-    engine.release();
-    super.onClose();
+  Future<void> checkFollowers() async {
+    var doc = await FirebaseFirestore.instance
+        .collection("userProfile")
+        .doc(arg["uid"])
+        .collection("Followers")
+        .doc(currenduser)
+        .get();
+    isfollowing.value = doc.exists;
+  }
+  //below are related comments
+
+  RxString commentuser = "Guest".obs;
+  Future<void> commentUsers() async {
+    var doc = await FirebaseFirestore.instance
+        .collection("userProfile")
+        .doc(currenduser)
+        .get();
+    if (doc.exists) {
+      commentuser.value = doc.data()?["name"] ?? "No Name";
+    }
+  }
+
+  Future<void> sendComment() async {
+    if (commentController.text.isEmpty) return;
+    String comment = commentController.text.toString();
+    commentController.clear();
+    try {
+      await FirebaseFirestore.instance
+          .collection("LiveStream")
+          .doc(arg["uid"])
+          .collection("Comments")
+          .add({
+            "userName": commentuser.value,
+            "userId": currenduser,
+            "comment": comment,
+            "sendAt": FieldValue.serverTimestamp(),
+          });
+    } catch (e) {
+      debugPrint("Comment error: $e");
+    }
   }
 }
--- lib/App/AppUi/LiveStreaming/WatchStreaming/watchstreaming_class.dart
+++ lib/App/AppUi/LiveStreaming/WatchStreaming/watchstreaming_class.dart
@@ -1,9 +1,9 @@
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
+import 'package:pandlive/App/AppUi/LiveStreaming/WatchStreaming/watch_stream_controllers.dart';
 import 'package:pandlive/App/Widgets/TextFields/textfield.dart';
 
 import 'package:pandlive/Utils/Constant/app_heightwidth.dart';
@@ -19,8 +19,6 @@ class WatchstreamingClass extends StatefulWidget {
 
 class _WatchstreamingClassState extends State<WatchstreamingClass> {
   final arg = Get.arguments;
-  RxBool isfollowing = false.obs;
-  TextEditingController commentController = TextEditingController();
 
   late RtcEngine _engine;
   final String appId = "5eda14d417924d9baf39e83613e8f8f5";
@@ -102,129 +100,7 @@ class _WatchstreamingClassState extends State<WatchstreamingClass> {
     }
   }
 
-  final currenduser = FirebaseAuth.instance.currentUser!.uid;
-  Future<void> checkFollowers() async {
-    var doc = await FirebaseFirestore.instance
-        .collection("userProfile")
-        .doc(arg["uid"])
-        .collection("Followers")
-        .doc(currenduser)
-        .get();
-    isfollowing.value = doc.exists;
-  }
-
-  Future<void> toggleFollow() async {
-    // 1. Instantly flip the UI button state
-    isfollowing.toggle();
-
-    // 2. Setup references for the Host (The person being watched)
-    var hostProfileRef = FirebaseFirestore.instance
-        .collection("userProfile")
-        .doc(arg["uid"]);
-    var hostFollowersSub = hostProfileRef
-        .collection("Followers")
-        .doc(currenduser);
-
-    // 3. Setup references for Me (The person watching)
-    var myProfileRef = FirebaseFirestore.instance
-        .collection("userProfile")
-        .doc(currenduser);
-    var myFollowingSub = myProfileRef.collection("Following").doc(arg["uid"]);
-
-    try {
-      if (isfollowing.value) {
-        // --- ACTION: FOLLOW ---
-
-        // Add me to the Host's "Followers" list
-        await hostFollowersSub.set({
-          "followerId": currenduser,
-          "followAt": FieldValue.serverTimestamp(),
-        });
-        // Add the Host to my "Following" list
-        await myFollowingSub.set({
-          "hostId": arg["uid"],
-          "followAt": FieldValue.serverTimestamp(),
-        });
-
-        // Update the totals on the main profile documents (Safe create if missing)
-        await hostProfileRef.set({
-          "totalFollowers": FieldValue.increment(1),
-        }, SetOptions(merge: true));
-        await myProfileRef.set({
-          "totalFollowing": FieldValue.increment(1),
-        }, SetOptions(merge: true));
-      } else {
-        // --- ACTION: UNFOLLOW ---
-        await hostFollowersSub.delete();
-        await myFollowingSub.delete();
-
-        // Get the current count first to make sure we don't go below 0
-        var hostDoc = await hostProfileRef.get();
-        int currentHostFollowers = hostDoc.data()?['totalFollowers'] ?? 0;
-
-        // Only decrement if the count is greater than 0
-        if (currentHostFollowers > 0) {
-          await hostProfileRef.set({
-            "totalFollowers": FieldValue.increment(-1),
-          }, SetOptions(merge: true));
-        } else {
-          // If it's already 0 or negative, force it to stay at 0
-          await hostProfileRef.set({
-            "totalFollowers": 0,
-          }, SetOptions(merge: true));
-        }
-
-        // Do the same for your "following" count
-        var myDoc = await myProfileRef.get();
-        int currentMyFollowing = myDoc.data()?['totalFollowing'] ?? 0;
-
-        if (currentMyFollowing > 0) {
-          await myProfileRef.set({
-            "totalFollowing": FieldValue.increment(-1),
-          }, SetOptions(merge: true));
-        } else {
-          await myProfileRef.set({
-            "totalFollowing": 0,
-          }, SetOptions(merge: true));
-        }
-      }
-    } catch (e) {
-      // If something goes wrong (no internet, etc), flip the button back
-      isfollowing.toggle();
-      debugPrint("Follow Error: $e");
-    }
-  }
-
-  RxString commentuser = "Guest".obs;
-  Future<void> commentUsers() async {
-    var doc = await FirebaseFirestore.instance
-        .collection("userProfile")
-        .doc(currenduser)
-        .get();
-    if (doc.exists) {
-      commentuser.value = doc.data()?["name"] ?? "No Name";
-    }
-  }
-
-  Future<void> sendComment() async {
-    if (commentController.text.isEmpty) return;
-    String comment = commentController.text.toString();
-    commentController.clear();
-    try {
-      await FirebaseFirestore.instance
-          .collection("LiveStream")
-          .doc(arg["uid"])
-          .collection("Comments")
-          .add({
-            "userName": commentuser.value,
-            "userId": currenduser,
-            "comment": comment,
-            "sendAt": FieldValue.serverTimestamp(),
-          });
-    } catch (e) {
-      debugPrint("Comment error: $e");
-    }
-  }
+  final streamcontroll = Get.find<WatchStreamControllers>();
 
   //now get comment
   var getComment = FirebaseFirestore.instance.collection("LiveStream");
@@ -233,8 +109,8 @@ class _WatchstreamingClassState extends State<WatchstreamingClass> {
   void initState() {
     super.initState();
     Future.microtask(() async {
-      await commentUsers();
-      await checkFollowers();
+      await streamcontroll.commentUsers();
+      await streamcontroll.checkFollowers();
     });
     _commentStream = FirebaseFirestore.instance
         .collection("LiveStream")
@@ -253,7 +129,7 @@ class _WatchstreamingClassState extends State<WatchstreamingClass> {
 
     _engine.leaveChannel();
     _engine.release();
-    commentController.dispose();
+    streamcontroll.commentController.dispose();
     super.dispose();
   }
 
@@ -339,10 +215,10 @@ class _WatchstreamingClassState extends State<WatchstreamingClass> {
                       ),
                     ),
                     onPressed: () {
-                      toggleFollow();
+                      streamcontroll.toggleFollow();
                     },
                     child: Obx(
-                      () => isfollowing.value
+                      () => streamcontroll.isfollowing.value
                           ? Text(
                               isArabic ? "التالي" : "Following",
                               style: isArabic
@@ -458,7 +334,7 @@ class _WatchstreamingClassState extends State<WatchstreamingClass> {
               children: [
                 Expanded(
                   child: MyTextFormField(
-                    controller: commentController,
+                    controller: streamcontroll.commentController,
                     keyboard: TextInputType.text,
                     hintext: isArabic
                         ? "اكتب تعليقاً..."
@@ -468,7 +344,7 @@ class _WatchstreamingClassState extends State<WatchstreamingClass> {
                 IconButton(
                   style: IconButton.styleFrom(backgroundColor: Colors.black54),
                   onPressed: () {
-                    sendComment();
+                    streamcontroll.sendComment();
                   },
                   icon: Icon(Icons.send_rounded, color: Colors.white),
                 ),
