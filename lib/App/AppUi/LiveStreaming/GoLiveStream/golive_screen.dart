d39e1d10c073837297cabb45f304fb9ff0d4c451
arshadflutterdev <arshadcodess@gmail.com>
2026-01-25 12:29:11 +0500

    changes

--- lib/App/AppUi/LiveStreaming/GoLiveStream/golive_screen.dart
+++ lib/App/AppUi/LiveStreaming/GoLiveStream/golive_screen.dart
@@ -56,7 +56,7 @@ class _GoliveScreenState extends State<GoliveScreen>
   late RtcEngine _engine;
   final String appId = "5eda14d417924d9baf39e83613e8f8f5";
   final String tempToken =
-      "007eJxTYDibmmPeJyS0L9I5avqitZY2J0sdQvZlfo9IvvrxdJfzHykFBssU4yRzc9M0Y2PLFJOUxDSLJEPTNMNk8xSD5GTzNOO0tKjizIZARoZlS8VYGRkgEMTnYyhJLS7JzEt3zkjMy0vNYWAAAKkwI8M=";
+      "007eJxTYKhpaqydHvbuTO70p/EHWyRDX9g+288WUN1d17/jidGqpmQFBssU4yRzc9M0Y2PLFJOUxDSLJEPTNMNk8xSD5GTzNOO0oqOlmQ2BjAw9P+4yMjJAIIjPx1CSWlySmZfunJGYl5eaw8AAAH0YJrE=";
   // final String channelName = "testingChannel";
   VideoViewController? _localviewController;
 
