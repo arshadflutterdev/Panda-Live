d76f04cb3589eb399719b2dd230baa6d5dd3f9b5
arshadflutterdev <arshadcodess@gmail.com>
2026-01-26 19:21:32 +0500

    changes

--- lib/App/AppUi/BottomScreens/ProfileScreens/follower_list.dart
+++ lib/App/AppUi/BottomScreens/ProfileScreens/follower_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/route_manager.dart';
 
 class FollowerListScreen extends StatefulWidget {
   const FollowerListScreen({super.key});
@@ -8,8 +10,9 @@ class FollowerListScreen extends StatefulWidget {
 }
 
 class _FollowerListScreenState extends State<FollowerListScreen> {
+  final arg = Get.arguments;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar: AppBar(title: Text("${arg["followerss"]}")));
   }
 }
