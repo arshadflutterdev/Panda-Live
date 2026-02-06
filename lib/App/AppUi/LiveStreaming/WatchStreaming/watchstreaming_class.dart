dd51c0dd616fd3b8f73b59df65fadb9030e14472
arshadflutterdev <arshadcodess@gmail.com>
2026-02-06 21:19:22 +0500

    changes

--- lib/App/AppUi/LiveStreaming/WatchStreaming/watchstreaming_class.dart
+++ lib/App/AppUi/LiveStreaming/WatchStreaming/watchstreaming_class.dart
@@ -24,7 +24,7 @@ class _WatchstreamingClassState extends State<WatchstreamingClass> {
   final String appId = "5eda14d417924d9baf39e83613e8f8f5";
   final String channelName = "testingChannel";
   final String appToken =
-      "007eJxTYMisq7Jd9ub3UoFWW66CevYlBX/OukTz2IiHMH3sdRd1vqDAYJlinGRubppmbGyZYpKSmGaRZGiaZphsnmKQnGyeZpxmxdWc2RDIyFD98hMzIwMEgvh8DCWpxSWZeenOGYl5eak5DAwAI0UiVQ==";
+      "007eJxTYLj+6dy8P7+qr0ovSwqOO8idLGN/0jF1Ma8v+5KC26cFf69VYLBMMU4yNzdNMza2TDFJSUyzSDI0TTNMNk8xSE42TzNOCxdpy2wIZGQw/HuHhZEBAkF8PoaS1OKSzLx054zEvLzUHAYGAD33JTM=";
 
   var remoteviewController = Rxn<VideoViewController>();
   Future<void> joinasaudi() async {
