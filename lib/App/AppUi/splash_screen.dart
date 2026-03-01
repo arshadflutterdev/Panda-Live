9b0c5d99d7587712c0a299e49082704eba56df52
arshadflutterdev <arshadcodess@gmail.com>
2026-03-02 03:07:35 +0500

    changes

--- lib/App/AppUi/splash_screen.dart
+++ lib/App/AppUi/splash_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pandlive/App/Routes/app_routes.dart';
 import 'package:pandlive/Utils/Constant/app_heightwidth.dart';
+import 'package:pandlive/Utils/Constant/app_images.dart';
 import 'package:pandlive/Utils/Constant/app_style.dart';
 import 'package:get/get.dart';
 
@@ -20,38 +21,38 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () async {
-      if (currentuser != null) {
-        try {
-          DocumentSnapshot userData = await FirebaseFirestore.instance
-              .collection("userProfile")
-              .doc(currentuser!.uid)
-              .get();
-          if (userData.exists) {
-            var data = userData.data() as Map<String, dynamic>;
-            if (data["blockStatus"] == "blocked") {
-              await FirebaseAuth.instance.signOut();
-              Get.offAllNamed(AppRoutes.authoptions);
-              Get.snackbar(
-                "Account Suspended",
-                "Your account has been blocked by the admin. Please contact support for further details.",
-                backgroundColor: Colors.red,
-                colorText: Colors.white,
-              );
-            } else {
-              Get.offAllNamed(AppRoutes.bottomnav);
-            }
-          } else {
-            Get.offAllNamed(AppRoutes.authoptions);
-          }
-        } catch (e) {
-          debugPrint("Error: $e");
-          Get.offAllNamed(AppRoutes.authoptions);
-        }
-      } else {
-        Get.offAllNamed(AppRoutes.authoptions);
-      }
-    });
+    // Timer(Duration(seconds: 3), () async {
+    //   if (currentuser != null) {
+    //     try {
+    //       DocumentSnapshot userData = await FirebaseFirestore.instance
+    //           .collection("userProfile")
+    //           .doc(currentuser!.uid)
+    //           .get();
+    //       if (userData.exists) {
+    //         var data = userData.data() as Map<String, dynamic>;
+    //         if (data["blockStatus"] == "blocked") {
+    //           await FirebaseAuth.instance.signOut();
+    //           Get.offAllNamed(AppRoutes.authoptions);
+    //           Get.snackbar(
+    //             "Account Suspended",
+    //             "Your account has been blocked by the admin. Please contact support for further details.",
+    //             backgroundColor: Colors.red,
+    //             colorText: Colors.white,
+    //           );
+    //         } else {
+    //           Get.offAllNamed(AppRoutes.bottomnav);
+    //         }
+    //       } else {
+    //         Get.offAllNamed(AppRoutes.authoptions);
+    //       }
+    //     } catch (e) {
+    //       debugPrint("Error: $e");
+    //       Get.offAllNamed(AppRoutes.authoptions);
+    //     }
+    //   } else {
+    //     Get.offAllNamed(AppRoutes.authoptions);
+    //   }
+    // });
   }
 
   @override
@@ -59,7 +60,14 @@ class _SplashScreenState extends State<SplashScreen> {
     double height = AppHeightwidth.screenHeight(context);
     double width = AppHeightwidth.screenWidth(context);
     return Scaffold(
-      body: Center(child: Text("PandaLive", style: AppStyle.logo)),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(AppImages.splashScren),
+          ),
+        ),
+      ),
     );
   }
 }
