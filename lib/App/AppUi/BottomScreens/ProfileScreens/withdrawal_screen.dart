743dab887d374795d0da95ecdae99464d0337caa
arshadflutterdev <arshadcodess@gmail.com>
2026-03-07 00:24:24 +0500

    changes

---
+++ lib/App/AppUi/BottomScreens/ProfileScreens/withdrawal_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WithdrawalScreen extends StatefulWidget {
+  const WithdrawalScreen({super.key});
+
+  @override
+  State<WithdrawalScreen> createState() => _WithdrawalScreenState();
+}
+
+class _WithdrawalScreenState extends State<WithdrawalScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
