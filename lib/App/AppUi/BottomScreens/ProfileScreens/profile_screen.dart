10f95dac82994ffcdcff5b5c346540627778d482
arshadflutterdev <arshadcodess@gmail.com>
2026-03-07 00:12:54 +0500

    changes

--- lib/App/AppUi/BottomScreens/ProfileScreens/profile_screen.dart
+++ lib/App/AppUi/BottomScreens/ProfileScreens/profile_screen.dart
@@ -470,7 +470,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                 : "Convert 45000 Coins To 45\$",
                             confirm: TextButton(
                               onPressed: () async {
-                                if (awardCoins.value >= 45000) {
+                                if (awardCoins.value >= 50) {
                                   try {
                                     final uid =
                                         FirebaseAuth.instance.currentUser!.uid;
